f1205590cab36244b62e56504b72e249f551960e
saintvrai <ishanovairat@gmail.com>
2022-11-12 19:40:55 +0300

    DC12| handle func settings

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+)
+
+type Car struct {
+	ID       string    `json:"id"`
+	Name     string    `json:"name"`
+	CarBrand *CarBrand `json:"carBrand"`
+}
+type CarBrand struct {
+	Name string `json:"name"`
+}
+
+var cars []Car
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/cars", getCars).Methods("GET")
+	r.HandleFunc("/cars/{id}", getCars).Methods("GET")
+	r.HandleFunc("/cars", createCar).Methods("POST")
+	r.HandleFunc("/cars/{id}", updateCar).Methods("PUT")
+	r.HandleFunc("/cars", deleteCar).Methods("DELETE")
 	fmt.Printf("Hello World")
 	fmt.Printf("Hello World")
 }
