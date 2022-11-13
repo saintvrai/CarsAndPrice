3a3ce82530c14eda0680dca7c878b8109d0211aa
saintvrai <ishanovairat@gmail.com>
2022-11-13 16:55:36 +0300

    DC12 | create CRUD functions

--- main.go
+++ main.go
@@ -96,6 +96,7 @@ func main() {
 	r.HandleFunc("/cars", createCar).Methods("POST")
 	r.HandleFunc("/cars/{id}", updateCar).Methods("PUT")
 	r.HandleFunc("/cars", deleteCar).Methods("DELETE")
+
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
